07f3e4f89e3d4deb6d0dd80bc93603cdef4641b1
joshuapurba <84613341+joshuapurba@users.noreply.github.com>
2021-05-27 20:40:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelector(".merah").addEventListener("click", function(){
+    const body = document.querySelector("body")
+    body.style.background = "red" 
+    
+})
+
+document.querySelector(".kuning").addEventListener("click", function(){
+    const body = document.querySelector("body")
+    body.style.background = "yellow" 
+    
+})
+
+document.querySelector(".hijau").addEventListener("click", function(){
+    const body = document.querySelector("body")
+    body.style.background = "green" 
+    
+})
+
+document.querySelector(".biru").addEventListener("click", function(){
+    const body = document.querySelector("body")
+    body.style.background = "blue" 
+    
+})
+
+document.querySelector(".abu").addEventListener("click", function(){
+    const body = document.querySelector("body")
+    body.style.background = "grey" 
+    
+})
+
